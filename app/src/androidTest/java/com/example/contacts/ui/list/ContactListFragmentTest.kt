8f92cefa7832de536d8d66a121a23eec1129c0c1
Kun <kun.register@gmail.com>
2019-05-26 05:49:21 +0100

    markoff unused code

--- app/src/androidTest/java/com/example/contacts/ui/list/ContactListFragmentTest.kt
+++ app/src/androidTest/java/com/example/contacts/ui/list/ContactListFragmentTest.kt
@@ -1,6 +1,5 @@
 package com.example.contacts.ui.list
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.filters.SmallTest
 import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
@@ -13,6 +12,6 @@ class ContactListFragmentTest {
     @Test
     fun test() {
         // TODO Apply Fragment Factory to instantiate Fragment with designated properties
-        launchFragmentInContainer<ContactListFragment>()
+//        launchFragmentInContainer<ContactListFragment>()
     }
 }
